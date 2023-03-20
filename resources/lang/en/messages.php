752f803b04d4402fa6bfda37d1a1137e23162b66
Luan Pham <luanpm88@gmail.com>
2023-03-20 13:54:19 +0700

    typo

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -7,7 +7,10 @@ return array(
     'connection' => 'Connect to ChatGPT',
     'settings.intro' => 'This plugin adds the power of ChatGPT to the Acelle Mail application to assist customers
         in getting help with various tasks, such as recommending email subjects,
-        improving email content, and much more. Fill your Chatgpt OpenAI API Key below to connect.
+        improving email content, and much more. Fill your Chatgpt OpenAI API Key below to connect.<br>
+        If you have not yet obtained an OpenAI API Key, please
+        <a target="_blank" href="https://platform.openai.com/account/api-keys">visit the
+            OpenAI API Dashboard</a> to get one.
     ',
     'save' => 'Save',
     'cancel' => 'Cancel',
